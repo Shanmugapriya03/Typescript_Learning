d7c90169511072169ba258354f9d790405f97588
priya <priya97a@gmail.com>
2017-08-08 22:59:47 +0530

    assertion and arrow functions

---
+++ arrow.js
@@ -0,0 +1,8 @@
+var log = function (message) {
+    console.log(message);
+};
+var dolog = function (message) {
+    console.log(message);
+};
+var logg = function (message) { return console.log(message); };
+var noParam = function () { return console.log("hi dear!"); };
---
+++ arrow.ts
@@ -0,0 +1,11 @@
+let log = function(message){
+    console.log(message);
+}
+
+let dolog=(message)=>{
+    console.log(message);
+}
+
+let logg=message=>console.log(message);
+
+let noParam=()=>console.log("hi dear!");
